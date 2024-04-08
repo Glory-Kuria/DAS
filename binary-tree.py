76b6350ef45db04d51710fa77d188be67519043f
Glory_Kuria <kuriaglory@gmail.com>
2024-04-08 19:28:24 +0300

    add file

--- binary-tree.py
+++ binary-tree.py
@@ -45,47 +45,47 @@ if __name__ == '__main__':
     
     
     
-    # Write a function to determine the last element in a binary tree
-    #Represent a node of binary tree  
+    # Write a function to determine the largest  element in a binary tree
+
 class Node:  
     def __init__(self,data):  
-        #Assign data to the new node, set left and right children to None  
+         
         self.data = data;  
         self.left = None;  
         self.right = None;  
    
 class LargestNode:  
     def __init__(self):  
-        #Represent the root of binary tree  
+         
         self.root = None;  
           
-    #largestElement() will find out the largest node in the binary tree  
+     
     def largestElement(self, temp):  
-        #Check whether tree is empty  
+        
         if(self.root == None):  
             print("Tree is empty");  
             return 0;  
               
         else:      
-            #Variable maximum will store temp's data  
+             
             maximum = temp.data;  
               
-            #It will find largest element in left subtree  
+            
             if(temp.left != None):  
                 leftMax = self.largestElement(temp.left);  
-                #Compare variable maximum with leftMax and store greater value into maximum  
+                
                 maximum = max(maximum, leftMax);  
               
-            #It will find largest element in right subtree  
+             
             if(temp.right != None):  
                 rightMax = self.largestElement(temp.right);  
-                #Compare variable maximum with rightMax and store greater value into maximum  
+                  
                 maximum = max(maximum, rightMax);  
               
             return maximum;  
    
 bt = LargestNode();  
-#Add nodes to the binary tree  
+
 bt.root = Node(15);  
 bt.root.left = Node(30);  
 bt.root.right = Node(55);  
@@ -93,5 +93,5 @@ bt.root.left.left = Node(70);
 bt.root.right.left = Node(60);  
 bt.root.right.right = Node(6);  
    
-#Display largest node in the binary tree  
+
 print("Largest element in the binary tree: " + str(bt.largestElement(bt.root)));  
